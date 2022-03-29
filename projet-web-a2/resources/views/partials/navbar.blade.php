a1c3c764086a87ccb74f3faa6772e52ffa2e647a
DevllIA <anthony.rambaut@viacesi.fr>
2022-03-29 10:47:32 +0200

    Partials : Navbar v1
    Première version de la navbar en attente d'adaptation par rapport aux views et aux routes

---
+++ projet-web-a2/resources/views/partials/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home Page</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Search</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Sign-in</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Sign-up</a>
+        </li>
+    </ul>
+</nav>
