4252976e0ee150e66a6b0038922c3cb068fda1c6
DevllIA <anthony.rambaut@viacesi.fr>
2022-03-29 15:01:39 +0200

    Navbar v2
    Navbar v2, manque les views

--- projet-web-a2/resources/views/partials/navbar.blade.php
+++ projet-web-a2/resources/views/partials/navbar.blade.php
@@ -1,16 +1,20 @@
-<nav>
-    <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home Page</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Search</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Sign-in</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Sign-up</a>
-        </li>
-    </ul>
-</nav>
+<div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <img src="https://media.discordapp.net/attachments/899570915212402740/958341950250057748/logo.png?width=1440&height=606" alt="CESI INTERNSHIP" style="width:50%">
+        </a>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+            <li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
+            <li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
+            <li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
+            <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+        </ul>
+
+        <div class="col-md-3 text-end">
+            <button type="button" class="btn btn-outline-primary me-2">Login</button>
+            <button type="button" class="btn btn-primary">Sign-up</button>
+        </div>
+    </header>
+  </div>
